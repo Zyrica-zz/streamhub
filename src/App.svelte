18cfe384eebdf8cbb435f7cdcb42bd1dd537055f
Olof Drevin <sknoll@gmail.com>
2020-05-28 16:22:18 +0200

    Twitch embedded test

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,73 @@
 <script>
   import { get } from 'axios'
+  import Menu from './menu.svelte'
   let streams = []
+
+  const minWidth = 400
+
+  const aspectRatio = 1920/1080
+
+  let columns
+  let width
+  let height
+
+  function onResize() {
+    const ele = document.getElementById('streams')
+    if (!ele) {
+      requestAnimationFrame(onResize)
+      return;
+    }
+    const containerWidth = ele.offsetWidth
+
+    columns = Math.floor(containerWidth/minWidth)
+    if (columns < 0) columns = 1
+    if (columns > 4) columns = 4
+    width = containerWidth / columns
+    height = width / aspectRatio
+  }
+  window.addEventListener('resize', onResize)
+  onResize()
+
+  function activate(id) {
+    if (!document.getElementById(''+id)) {
+      requestAnimationFrame(() => {
+        activate(id);
+      })
+    } else {
+      const { channel } = streams.find(stream => stream._id === id)
+      const result = new Twitch.Embed(''+id, {
+        width,
+        height,
+        channel: channel.name,
+        layout: 'video',
+      })
+      result.setMuted(true)
+    }
+  }
+
   async function init() {
     const { data } = await get('/api')
-    streams = data
-    console.log(streams[0].channel)
+    streams = data.splice(0, 12)
+    streams.forEach(stream => activate(stream._id))
   }
   init()
 
 </script>
 
-<style>
-  .streams {
-    display: flex;
-    flex-direction: column;
-  }
-  .stream {
-    display: flex;
-  }
-</style>
-
-<div class="streams">
-  {#each streams as { preview, channel }}
-    <div class="stream">
-      <img alt="" src={preview.medium} />
-      <div>
-        <h2>{channel.display_name}</h2>
-        <h3>{channel.description}</h3>
-      </div>
+<Menu />
+<div id="streams">
+  {#each streams as { _id, preview, channel }, i}
+    <div id={_id} style="
+      width: {width}px;
+      height: {height}px;
+    ">
     </div>
   {/each}
 </div>
+
+<style>
+  #streams {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
---
+++ src/menu.svelte
@@ -0,0 +1,41 @@
+
+<div class="menu">
+  <input placeholder="Search" />
+  <button>
+    <img src="/search.png" alt="Search" width="18px" />
+  </button>
+</div>
+
+<style>
+  .menu {
+    display: flex;
+    padding: 0.5em;
+    background-color: rgb(24,24,27);
+    justify-content: center;
+    border-bottom: 2px solid black;
+  }
+  input {
+    background-color: rgb(58,58,61);
+    color: rgb(108,108,110);
+    padding: 0.5em;
+    font-size: 1em;
+    font-weight: bold;
+    font-family: Roobert, "Helvetica Neue", Helvetica, Arial, sans-serif;
+    border: 1px solid black;
+    border-top-left-radius: 0.5em;
+    border-bottom-left-radius: 0.5em;
+  }
+  input:focus {
+    border-color: lime;
+    outline: none
+  }
+  button {
+    background-color: rgb(41,41,44);
+    border: 1px solid black;
+    border-left: none;
+    border-top-right-radius: 0.5em;
+    border-bottom-right-radius: 0.5em;
+    padding: 0.5em;
+    padding-right: 0.75em;
+  }
+</style>
