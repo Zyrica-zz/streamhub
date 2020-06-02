bc623963626314aa96473032ee36b2b68e28d5c5
Olof Drevin <sknoll@gmail.com>
2020-06-02 16:16:29 +0200

    Favourites now in local storage.

--- src/favourites-store.js
+++ src/favourites-store.js
@@ -1,10 +1,13 @@
 import { derived, readable } from 'svelte/store'
 
-const favourites = new Set()
+const key = 'favourites'
+
+const favourites = loadFromLocalStorage()
 
 let save
 const readableFavorites = readable(favourites, set => {
   save = () => {
+    saveToLocalStorage()
     set(favourites)
   }
 })
@@ -22,3 +25,11 @@ export const isFavourite = derived(
   readableFavorites,
   favourites => id => favourites.has(id)
 )
+
+function saveToLocalStorage() {
+  localStorage.setItem(key, JSON.stringify([...favourites]))
+}
+function loadFromLocalStorage() {
+  let json = localStorage.getItem(key)
+  return  new Set(json ? JSON.parse(json) : [])
+}
